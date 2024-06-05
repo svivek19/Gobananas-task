16f0729c7123ab3f3cb85254be15728368b33c5a
svivek19 <svivek030503@gmail.com>
2024-06-05 19:23:26 +0530

    bug fix

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="fixed w-full z-20 bg-[#222b38] top-0 start-0">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <h1 className="font-bold text-white text-2xl">FlowLaunch</h1>
+        <h1 className="font-bold text-white text-2xl">Gobananas</h1>
       </div>
     </nav>
   );
